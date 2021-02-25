529564223d779bdf001604a5de2304d7c43524ef
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-25 15:05:12 +0200

    Updated Grid - Lecture 4

--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -7,17 +7,19 @@
 
 import SwiftUI
 
-struct Grid<Item, ItemView>: View {
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     
     var items: [Item]
     var viewForItem: (Item) -> ItemView
     
-    init(_ item: [Item], viewForItem: @escaping (Item) -> ItemView) {
-        self.items = item
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
         self.viewForItem = viewForItem
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ForEach(items) { item in
+            viewForItem(item)
+        }
     }
 }
