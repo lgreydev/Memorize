a4417fd1bffc1bcd1b068ffcccce2466541a37bb
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-25 09:58:59 +0200

    Added Grid

---
+++ Grid.swift
@@ -0,0 +1,28 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Sergey Lukaschuk on 25.02.2021.
+//
+
+import SwiftUI
+
+struct Grid<Item, ItemView>: View {
+    
+    var item: [Item]
+    var viewForItem: (Item) -> ItemView
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+
+
+
+
+
+
+
