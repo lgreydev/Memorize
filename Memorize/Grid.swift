8e0e1503349509927508fe839c9b7a3e680e9eed
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-25 16:35:52 +0200

    Updated Grid - Lecture 4

--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -18,8 +18,18 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
     
     var body: some View {
-        ForEach(items) { item in
-            viewForItem(item)
+        GeometryReader { geometry in
+            body(for: geometry.size)
         }
+    }
+    
+    func body(for size: CGSize) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: size)
+        }
+    }
+    
+    func body(for item: Item, in size: CGSize) -> some View {
+        return viewForItem(item)
     }
 }
