8a2b5e23a7487c6e7093f24cc86e4f22380fdf53
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-26 08:37:09 +0200

    Added new file Array+Identifiable - extension

---
+++ Memorize/Array+Identifiable.swift
@@ -0,0 +1,25 @@
+//
+//  Array+Identifiable.swift
+//  Memorize
+//
+//  Created by Sergey Lukaschuk on 26.02.2021.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int {
+        for index in 0..<self.count {
+            if self[index].id == matching.id {
+                return index
+            }
+        }
+        return 0 // TODO: bogus
+    }
+}
+
+
+
+
+
+
--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -37,16 +37,7 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
 }
 
-extension Array where Element: Identifiable {
-    func firstIndex(matching: Element) -> Int {
-        for index in 0..<self.count {
-            if self[index].id == matching.id {
-                return index
-            }
-        }
-        return 0 // TODO: bogus
-    }
-}
+
 
 
 
