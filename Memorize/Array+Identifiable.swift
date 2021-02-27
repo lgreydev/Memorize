d6f1cf0767db223ecf1ef369dec478ff3a74f1d2
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-27 10:17:48 +0200

    Fixed function (firstIndex)

--- Memorize/Array+Identifiable.swift
+++ Memorize/Array+Identifiable.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 extension Array where Element: Identifiable {
-    func firstIndex(matching: Element) -> Int {
+    func firstIndex(matching: Element) -> Int? {
         for index in 0..<self.count {
             if self[index].id == matching.id {
                 return index
             }
         }
-        return 0 // TODO: bogus
+        return nil
     }
 }
 
