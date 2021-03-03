459a7212e7ad278a92d8385578cf03c500ce194e
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-03 13:38:19 +0200

    Updated function 'body(for item: Item, in layout: GridLayout) -> some View' in structure 'Grid'

--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -30,9 +30,9 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
     
     func body(for item: Item, in layout: GridLayout) -> some View {
-        let index = items.firstIndex(matching: item)
-        return viewForItem(item)
-            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
-            .position(layout.location(ofItemAt: index))
+        let index = items.firstIndex(matching: item)!
+               return viewForItem(item)
+                    .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+                    .position(layout.location(ofItemAt: index))
     }
 }
