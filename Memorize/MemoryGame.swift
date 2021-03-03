ba11c5eff50d66d0413924e3f316eb3afaa0c1d8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-03 14:15:34 +0200

    Updated function 'choose' of structure 'MemoryGame'

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,13 +10,28 @@ import Foundation
 
 // Model
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     var cards: Array<Card>
     
+    var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
     mutating func choose(card: Card) {
-        print("card chosen \(card)")
-        if let chosenIndex = cards.firstIndex(matching: card) {
-            cards[chosenIndex].isFaceUp = !cards[chosenIndex].isFaceUp
+        if let chosenIndex = cards.firstIndex(matching: card),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp = true
         }
     }
     
@@ -32,7 +47,7 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         var content: CardContent
         var id: Int
