69709de90d57818f34be7beaf04fffdbc3928841
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-10 20:07:06 +0200

    Add view modifier 'Cardify'

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -32,32 +32,26 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     func body(for size: CGSize) -> some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+        if card.isFaceUp || !card.isMatched {
+            ZStack {
                 Pie(
                     startAngle: Angle(degrees: 0 - 90),
                     endAngle: Angle(degrees: 110 - 90),
                     clockwise: true
                 )
-                    .padding(5)
-                    .opacity(0.4)
+                .padding(5)
+                .opacity(0.4)
                 Text(card.content)
-            } else {
-                if !card.isMatched {
-                    RoundedRectangle(cornerRadius: cornerRadius).fill()
-                }
+                    .font(Font.system(size: fontSize(for: size)))
             }
+            .cardify(isFaceUp: card.isFaceUp)
         }
-        .font(Font.system(size: fontSize(for: size)))
     }
-    
+
    // MARK: Control Panel
     
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3.0
     private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.70
     }
