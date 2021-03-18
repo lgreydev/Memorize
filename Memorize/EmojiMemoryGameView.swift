5885e30665282dc7dfc9a61a8d0fc08750554fec
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-18 18:12:00 +0200

    Add button a new game

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -13,14 +13,20 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards) { card in
-                CardView(card: card).onTapGesture {
-                    viewModel.choose(card: card)
-                }
-                .padding(5)
+        VStack {
+            Grid(viewModel.cards) { card in
+                    CardView(card: card).onTapGesture {
+                        viewModel.choose(card: card)
+                    }
+                    .padding(5)
+            }
+                    .padding()
+                    .foregroundColor(Color.orange)
+            
+            Button("New Game") {
+                viewModel.resetGame()
+            }
         }
-                .padding()
-                .foregroundColor(Color.orange)
     }
 }
 
