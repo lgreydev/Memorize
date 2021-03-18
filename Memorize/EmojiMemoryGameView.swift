7771379efca67ba27cafd056aa39b4715662646f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-18 17:53:10 +0200

    Update rotation animation of emoji

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -45,8 +45,8 @@ struct CardView: View {
                 .opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
-                    .rotationEffect(Angle.degrees(card.isMatched ? 180 : 0))
-                    .animation(Animation.linear(duration: 1))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
